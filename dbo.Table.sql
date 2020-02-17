05c982235413215b5fb652d9607192e75e0ea1d0
Frank Dahncke <MechWarrior@gmx.com>
2020-02-17 11:47:55 +0100

    update

---
+++ dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblParticipants]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+    [City] NVARCHAR(50) NULL
+)
--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 using namespace System;
-
+using namespace System::Data::SqlClient;
 int main() {
 	Console::WriteLine("Database CLR 17-FEB-2020");
+
+	// connection string. remember to escape the backslashes!
+	// NOTE: adapt path for your machine
+	String^ strConn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Alfa\\Desktop\\VC++\\DB-ADO\\DB-Example.mdf;Integrated Security=True;Connect Timeout=30";
+
+	SqlConnection^ connection = gcnew SqlConnection(strConn);
+
+	try {
+		connection->Open();
+	}
+	catch (Exception ^ ex) {
+		Console::WriteLine("ERROR: connection failed.\nReason: " + ex->Message);
+		Console::Write("Hit any key");
+		Console::ReadKey();
+		return -1;
+	}
+
+	Console::WriteLine("Connection successful");
+
+	String^ strCommand = "SELECT COUNT (*) FROM tblParticipants";// SQL command
+	SqlCommand^ command = gcnew SqlCommand(strCommand, connection);
+
+	Console::WriteLine(command->ExecuteScalar()); // for single line answer
+
+	connection->Close();
+
+	Console::Write("Hit any key");
 	Console::ReadKey();
 
 	return 0;
